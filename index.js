d3e2a9a86c17c57c60bccfa80e9e12857bdada2b
Niko Vierikko <niko@camoline.fi>
2020-06-06 01:32:28 +0300

    Tasks 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', function (req, res) {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body)
+  }
+})
+app.use(morgan(':method :url :status :response-time ms :body'))
 
 let contacts = [
   {
