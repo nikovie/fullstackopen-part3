67f4e57a98555bbda16b888a03ef622ac4cceba5
Niko Vierikko <niko@camoline.fi>
2020-06-07 23:17:47 +0300

    Task 3.11
    static frontend, build & deploy scripts

---
+++ build/precache-manifest.b17962c38885ad86483a054df21a6927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65abcd30001f82478c1f7bbf4b3c15bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc1b35b100d833bf33b6",
+    "url": "/static/css/main.b8f5920f.chunk.css"
+  },
+  {
+    "revision": "be4f21b3e6389ed8f1b8",
+    "url": "/static/js/2.a5e0891a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5e0891a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1b35b100d833bf33b6",
+    "url": "/static/js/main.4facab51.chunk.js"
+  },
+  {
+    "revision": "a62e6ea7717c0cd4d2b3",
+    "url": "/static/js/runtime-main.c9af20ba.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
-
 app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) {
   if (req.method === "POST") {
