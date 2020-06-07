d069de1a4995185368edd931ce483bf3346057de
Niko Vierikko <niko@camoline.fi>
2020-06-07 22:19:33 +0300

    dunno

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # fullstackopen-part3
 
 Separate repository for phonebook backend
+
+[Link to Heroku](https://tranquil-retreat-83953.herokuapp.com)
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('body', function (req, res) {
   if (req.method === "POST") {
@@ -105,7 +108,7 @@ app.delete('/api/persons/:id', (req, res) => {
 * end of API
 */
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
