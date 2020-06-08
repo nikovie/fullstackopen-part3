d55fe21d11c41a72ad0720b53095633e918c65de
Niko Vierikko <niko@camoline.fi>
2020-06-09 00:12:47 +0300

    Task 3.19
    useCreateIndex true

--- models/person.js
+++ models/person.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 
 mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true)
 
 const url = process.env.MONGODB_URI
 
